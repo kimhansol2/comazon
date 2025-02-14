fac0d3535464471f46701bfc00ba908dad2b3a0a
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-02-14 11:03:53 +0900

    '신규테이블추가'

---
+++ app.js
@@ -0,0 +1,85 @@
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import * as dotenv from "dotenv";
+
+dotenv.config(); //.env 파일에 저장된 환경 변수를 로드한다.
+const prisma = new PrismaClient(); // prisma 클라이언트 초기화
+const app = express();
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.send(users);
+});
+
+app.get("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.user.findUnique({
+    where: { id },
+  });
+  res.send(user);
+});
+
+app.post("/users", async (req, res) => {
+  const user = await prisma.user.create({ data: req.body });
+  res.status(201).send(user);
+});
+
+app.patch("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  const user = await prisma.user.update({
+    where: { id },
+    data: req.body,
+  });
+  res.send(user);
+});
+
+app.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+  await prisma.user.delete({
+    where: { id },
+  });
+  res.send("Success delete");
+});
+
+app.get("/products", async (req, res) => {
+  const products = await prisma.product.findMany();
+  console.log(products);
+  res.send(products);
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await prisma.product.findUnique({
+    where: { id },
+  });
+  console.log(product);
+  res.send(product);
+});
+
+app.post("/products", async (req, res) => {
+  const product = await prisma.product.create({ data: req.body });
+  res.send(product);
+});
+
+app.patch("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await prisma.product.update({
+    where: { id },
+    data: req.body,
+  });
+
+  res.send(product);
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  await prisma.product.delete({
+    where: { id },
+  });
+  res.sendStatus(204);
+});
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
