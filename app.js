26eb00670d366e52138388851ec843137b282948
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-02-17 10:24:07 +0900

    '변경사항저장'

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import express from "express";
-import { PrismaClient } from "@prisma/client"; //prisma는 데이터베이스와 연결해 데이터를 쉽게 조회하거나 수정할 수 있도록
+// express라는 라이버리를 가져온다. 이 라이브러리는 우리가 웹 서버를 만들 수 있게 도와주는 도구
+// 예를 들어 우리가 사람들과 대화할 수 있는 대화방을 만들고, 그 대화방에 사람들이 요청을 보내고, 서버가 응답할 수 있게 해주는 도구
+import { PrismaClient } from "@prisma/client";
+//prisma는 데이터베이스와 대화할 수 있게 해주는 도구. 예를들어 우리가 사람들의 이름 나이 직업 등을 저장한 목록이
+// 있을 때 , prisma를 통해 이 데이터를 저장하거나 수정하거나 조회할 수 있다.
 import { assert } from "superstruct";
-import * as dotenv from "dotenv"; //.env 파일에 있는 환경 변수들을 불러온다. (비밀정보를 관리하는 도구)
+//assert는 우리가 보낸 데이터가 올바른 형식인지 확인하는 도구. 예를들어 우리가 이메일을 보낼 때 이메일 주소 형식이 맞는지
+// 체크하는 것처럼 보낸 데이터가 규칙에 맞는지 확인하는 역할을 한다.
+import * as dotenv from "dotenv";
+//.env 파일에 비밀 정보를 저장해두고 그정보를 코드에서 불러와서 사용하는 도구. 예를들어 비밀번호나 중요한 api 키를 코드에
+// 직접 넣지 않고 안전하게 관리 할 수 있게 도와줌
 import {
   CreateUser,
   PatchUser,
@@ -10,13 +18,15 @@ import {
 } from "./structs.js";
 import { PrismaClientValidationError } from "@prisma/client/runtime/library";
 
-dotenv.config(); //.env 파일에 저장된 환경 변수를 로드한다.
-const prisma = new PrismaClient(); // prisma 클라이언트 초기화
-const app = express(); // 서버 만들기
+dotenv.config(); //.env  파일에 저장된 환경 변수들을 불러오는 명령이다.
+const prisma = new PrismaClient(); // prisma라는 변수를 만들어서 데이터베이스와 연결하는 역할을 함
+const app = express(); // express로 웹 서버를 만든다. 이 서버는 요청을 보내면 처리하고 응답을 보내주는 역할을 한다.
+// 예를 들어, 웹사이트나 앱을 만들 때 서버가 있어야 그 위에서 데이터를 주고 받을 수 있다.
 app.use(express.json()); // 서버가 json 형식의 데이터를 받을 수 있게 해준다.
+// 예를들어 우리가  {"name":"john"}과 같은 데이터를 서버에 보낼 때, 서버가 이 데이터를 제대로 이해할 수
+// 수 있도록 도와준다.
 
-// asyncHandler 함수: 비동기 요청 핸들러에 대한 에러 처리를 제공
-// 비동기 작업이 있는 API에서 asyncHan
+// asyncHandler 함수: 비동기 요청 핸들러에 대한 에러 처리를 제공.
 function asyncHandler(handler) {
   return async function (req, res) {
     try {
@@ -24,14 +34,14 @@ function asyncHandler(handler) {
     } catch (e) {
       if (
         e.name === "StructError" ||
-        (e instanceof Prisma.PrismaClientKnownRequestError &&
-          e.code === "P2002") ||
+        (e instanceof Prisma.PrismaClientKnownRequestError && //데이터가 잘못된 형식일때 400 오류를 보내고 메시지에는 오류 내용이 담김
+          e.code === "P2002") || // 이미 존재하는 데이터를 다시 저장하려고 할 때 400 오류를 보낸다
         e instanceof PrismaClientValidationError
       ) {
         res.status(400).send({ message: e.message }); // 유효성 검사 오류
       } else if (
         e instanceof Prisma.PrismaClientKnownRequestError &&
-        e.code === "P2025"
+        e.code === "P2025" // id로 데이터를 찾으려고 했는데 없다면 404 오류
       ) {
         res.status(404).send({ message: e.message });
       } else {
@@ -46,6 +56,7 @@ app.get(
   "/users",
   asyncHandler(async (req, res) => {
     const { offset = 0, limit = 10, order = "newest" } = req.query;
+
     let orderBy;
     switch (order) {
       case "oldest":
